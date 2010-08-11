f5b72933985f59865a419af120e59da2de740804
Brandon Tilley <binarymuse@binarymuse.net>
2010-08-11 14:06:35 -0700

    Added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RailsBook::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
