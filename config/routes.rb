7455863ff13aa0ebcb29baf5b39fd48565ae8388
Brandon Tilley <binarymuse@binarymuse.net>
2010-08-11 15:13:47 -0700

    Added help page and refactored tests for pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ RailsBook::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
   get "pages/about"
+  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
