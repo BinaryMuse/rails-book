391122775fa7a1e84fd9d66ed8ca6125d4fca1c9
Brandon Tilley <binarymuse@binarymuse.net>
2010-08-12 12:24:33 -0700

    User model w/ passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Brandon Tilley"
+  user.email                 "binarymuse@binarymuse.net"
+  user.password              "password"
+  user.password_confirmation "password"
+end
