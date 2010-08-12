ebd629d17a777400720ca77dc724e27311be041f
Brandon Tilley <binarymuse@binarymuse.net>
2010-08-12 00:09:00 -0700

    Fleshed out tests for view

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,4 +27,18 @@ describe "LayoutLinks" do
     response.should have_selector 'title', :content => "Sign up"
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector 'title', :content => "About"
+    click_link "Help"
+    response.should have_selector 'title', :content => "Help"
+    click_link "Contact"
+    response.should have_selector 'title', :content => "Contact"
+    click_link "Home"
+    response.should have_selector 'title', :content => "Home"
+    click_link "Sign up now!"
+    response.should have_selector 'title', :content => "Sign up"
+  end
+
 end
