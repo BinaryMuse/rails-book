dcd18bb8ed77f1a73bc958c093352dd3c8f3f39f
Brandon Tilley <binarymuse@binarymuse.net>
2010-08-11 22:25:08 -0700

    Added title helper to determine title of static pages, added blueprint css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
