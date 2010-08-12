52b5bd7afe0539f2b91b3a474e999ae920f1caac
Brandon Tilley <binarymuse@binarymuse.net>
2010-08-11 23:59:27 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 RailsBook::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
