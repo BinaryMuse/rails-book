cfaaae2213c877731184c304ffdad276a4685fad
Brandon Tilley <binarymuse@binarymuse.net>
2010-08-13 19:56:01 -0700

    Replaced string.clear with string = ''

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,8 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       @title = "Sign up"
-      @user.password.clear
-      @user.password_confirmation.clear
+      @user.password = ""
+      @user.password_confirmation = ""
       render 'new'
     end
   end
